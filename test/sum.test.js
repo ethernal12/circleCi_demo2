49131514a2534eab31529610edf09bbd993be928
Danijel <korbar41@gmail.com>
2022-05-23 19:47:02 +0200

    Adding falsePos test

--- test/sum.test.js
+++ test/sum.test.js
@@ -10,6 +10,10 @@ describe('testing sum function', () => {
 
         assert.equal(sum(1,2),3);
     });
+    it("Should NOT add two numbers", () => {
+
+        assert.equal(sum(1,2),4);
+    });
     
 
  })
