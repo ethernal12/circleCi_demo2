dd392abc0e2ad0972a87592dd039fcf8141173bf
Danijel <korbar41@gmail.com>
2022-05-23 20:01:55 +0200

    Adding mul test

--- sum.js
+++ sum.js
@@ -1,5 +1,8 @@
 function sum (a,b) {
     return a+ b;
 }
+function mul (a,b) {
+    return a * b;
+}
 
-module.exports = sum;
+module.exports = sum , mul;
--- test/sum.test.js
+++ test/sum.test.js
@@ -1,6 +1,7 @@
 var assert = require('assert');
 
 const sum = require("../sum.js");
+const mul = require("../sum.js");
 
 
 describe('testing sum function', () => { 
@@ -10,7 +11,10 @@ describe('testing sum function', () => {
 
         assert.equal(sum(1,2),3);
     });
+    it("Should multiply two numbers", () => {
+
+        assert.equal(mul(2,2),4);
+    });
    
-    
 
  })
