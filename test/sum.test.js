6743169539b100f0c406fbdfc5c99defb174ed4c
Danijel <korbar41@gmail.com>
2022-05-23 18:55:39 +0200

    Adding test case

--- test/sum.test.js
+++ test/sum.test.js
@@ -9,6 +9,11 @@ describe('testing sum function', () => {
     it("Should add two numbers", () => {
 
         assert.equal(sum(1,2),3);
+    });
+    it("Should NOT add two numbers", () => {
+
+        assert.equal(sum(1,2),4);
     })
+
 
  })
